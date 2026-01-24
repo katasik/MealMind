5874792901e24e345c18a8dfab885502acf28fba
katasik <56645248+katasik@users.noreply.github.com>
2026-01-24 05:56:23 +0100

    Add webpack alias for @/ path resolution on Netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(process.cwd(), 'src');
+    return config;
+  },
 }
 
 module.exports = nextConfig
