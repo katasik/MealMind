2d1c4163d53565e025d89f78203447e29a3a482b
Kata Sik <katasik@Katas-MacBook-Pro.local>
2026-02-02 08:53:53 +0400

    security: Update React 19.2.4 and Next.js 16.1.6

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(process.cwd(), 'src');
-    return config;
-  },
+  turbopack: {},
 }
 
 module.exports = nextConfig
